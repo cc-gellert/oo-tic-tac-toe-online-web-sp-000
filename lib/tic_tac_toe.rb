ce28ce6e84c0ae8b45dd9b2a5c28aa2d75b37754
Chloe Gell <gellert.chloe@gmail.com>
2020-02-01 19:34:02 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -21,12 +21,20 @@ class TicTacToe
   end 
   
   def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts '-----------'
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts '-----------'
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts '-----------'
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts '-----------'
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
+  
+  def input_to_index(user_input)
+    user_input.to_i - 1
+  end
+  
+  def move(board, index, player)
+    board[index] = player
+  end
 end 
 
 
@@ -37,14 +45,6 @@ def play(board)
   elsif draw?(board)
     puts "Cat's Game!"
   end
-end
-
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts '-----------'
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts '-----------'
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 def valid_move?(board, index)
@@ -71,10 +71,6 @@ def over?(board)
   won?(board) || draw?(board)
 end
 
-def input_to_index(user_input)
-  user_input.to_i - 1
-end
-
 def turn(board)
   puts 'Please enter 1-9:'
   user_input = gets.strip
@@ -99,9 +95,7 @@ def turn_count(board)
   board.count { |token| token == 'X' || token == 'O' }
 end
 
-def move(board, index, player)
-  board[index] = player
-end
+
 
 def winner(board)
   if winning_combo = won?(board)
